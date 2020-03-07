7d718258220ec61588c8db6b7c8f561a677014b1
lavish <lav@exam.com>
2020-03-07 19:56:23 +0530

    changes in PORT

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.get("/log_form",function(req,res){
   res.sendFile(__dirname+"/log_form.html");
 });
 
-app.listen("3000",function(){
+let port=process.env.PORT;
+if( port == null || port=="" ){
+  port=3000;
+}
+app.listen(port,function(){
   console.log("Server is started");
 });
