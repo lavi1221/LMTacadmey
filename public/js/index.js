9dc9d2ad86693c8a7fc9d3db454744d158ce5171
lavish <lav@exam.com>
2020-05-23 15:56:46 +0530

    add favicon

--- public/js/index.js
+++ public/js/index.js
@@ -14,10 +14,16 @@ var header = document.getElementById("navi");
 var sticky = header.offsetTop;
 
 function myFunction() {
+  var ob =document.getElementById("blok");
+ var op =document.getElementById("blok1");
 if (window.pageYOffset > sticky) {
+  ob.style.display = 'block';
+  op.style.display = 'block';
  header.classList.add("sticky");
  header.classList.remove("navitt");
 } else {
+  ob.style.display ='none';
+  op.style.display ='none';
  header.classList.add("navitt");
  header.classList.remove("sticky");
 }
